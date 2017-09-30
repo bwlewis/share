3be3a6fe577ecb991d5c1636d76bf6838ae37778
bwlewis <blewis@illposed.net>
2017-09-30 05:40:49 -0400

    doc

--- R/methods.R
+++ R/methods.R
@@ -33,7 +33,8 @@ setAs("object_store", "environment", function(from)
   toenv(from)
 })
 
-#' Convert an object store connection object to an environment full of promises
+#' Convert an object store connection object to an environment of promises
+#' Objects are lazily retrieved from the object store using \code{\link{get()}} when accessed.
 #' @param x an object store, see \code{connect()}, \code{\link{setAs}}
 #' @param base base path
 #' @return an environment
